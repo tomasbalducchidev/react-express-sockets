0232c058a1855062c5241106f735edba282e9dca
tomasbalducchidev <81332234+tomasbalducchidev@users.noreply.github.com>
2022-11-17 12:20:09 -0300

    agrega configuracion express y socketio

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import morgan from "morgan";
+import { Server as SocketServer } from "socket.io";
+import http from "http";
+import cors from "cors";
+import { PORT } from "./config.js";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.use(cors());
+app.use(morgan("dev"));
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  socket.on("message", (message) => {
+    socket.broadcast.emit("message", {
+      body: message,
+      from: socket.id,
+    });
+  });
+});
+
+server.listen(PORT);
+console.log(`Server started on port ${PORT}`);
